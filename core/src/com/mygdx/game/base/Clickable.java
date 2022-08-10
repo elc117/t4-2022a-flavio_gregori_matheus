9871119c1d629f5573353b82d6d343cf5213f26b
Gregori Dallanora Rubin <gregori.rubin@gmail.com>
2022-08-10 17:26:58 -0300

    boost voador criado

--- core/src/com/mygdx/game/base/Clickable.java
+++ core/src/com/mygdx/game/base/Clickable.java
@@ -2,16 +2,26 @@ package com.mygdx.game.base;
 
 public abstract class Clickable {
     protected long currencyPerClick;
+    protected boolean isBoosted;
+
+    public void setBoosted(boolean isBoosted) {
+        this.isBoosted = isBoosted;
+    }
 
     public Clickable(long currencyPerClick) {
         this.currencyPerClick = currencyPerClick;
     }
 
     public long generate(Stock stock) {
-        return currencyPerClick;
+        if (isBoosted) {
+            return currencyPerClick * 2;
+        } else {
+            return currencyPerClick;
+        }
     }
 
     public void boost() {
         currencyPerClick *= 2;
     }
+
 }
